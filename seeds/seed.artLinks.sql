084766d41e969a646331da480d2c7778735992e7
Sawyer King <kingsawyer555@gmail.com>
2021-01-11 11:42:06 -0800

    tests added

---
+++ seeds/seed.artLinks.sql
@@ -0,0 +1,18 @@
+INSERT INTO art(link)
+VALUES 
+('https://www.instagram.com/p/CJhxO8QFTCI/'),
+('https://www.instagram.com/p/BcwF36oBKwh/'),
+('https://www.instagram.com/p/BcoU6i0BDYz/'),
+('https://www.instagram.com/p/BasC76Sh6-p/'),
+('https://www.instagram.com/p/BiQxXhIHSNX/'),
+('https://www.instagram.com/p/BclsJWnhnk5/'),
+('https://www.instagram.com/p/BauVs4WhLmF/'),
+('https://www.instagram.com/p/Bb_VTgnhfte/'),
+('https://www.instagram.com/p/B2xzgD6F9IR/'),
+('https://www.instagram.com/p/B7kdCNllyLw/'),
+('https://www.instagram.com/p/B7fQyOAlVO-/'),
+('https://www.instagram.com/p/B9ikVKil3-Q/'),
+('https://www.instagram.com/p/Bb_VTgnhfte/'),
+('https://www.instagram.com/p/Bdgbu7QB7om/'),
+('https://www.instagram.com/p/BiVqbMPhhRh/'),
+('https://www.instagram.com/p/Ba3C23ChBWP/')
---
+++ seeds/seed.users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(username,password)
+VALUES
+('default', 'Defaultpassword1')
